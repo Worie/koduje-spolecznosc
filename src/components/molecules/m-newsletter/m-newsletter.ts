6d9f3a9c78d008b83a3d88e6cbe82572c1003cca
Jakubem <jakubmx@gmail.com>
2018-03-22 21:54:08 +0100

    add newsletter component

---
+++ src/components/molecules/m-newsletter/m-newsletter.ts
@@ -0,0 +1,12 @@
+import { Component, Vue } from 'vue-property-decorator'
+import './m-newsletter.scss'
+
+@Component({
+  name: 'm-newsletter',
+  template: require('./m-newsletter.html'),
+  components: {
+  }
+})
+export class mNewsletter extends Vue {
+
+}
