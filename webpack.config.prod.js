8f9e14a77a6ba68b852041bbd54391decf5c2b9f
Jakubem <jakubmx@gmail.com>
2018-03-31 20:42:00 +0200

    add webpack prod task

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -75,7 +75,7 @@ module.exports = {
         keepClosingSlash: true,
         minifyJS: true,
         minifyCSS: true,
-        minifyURLs: true
+        minifyURLs: true,
       }
     }),
     new PrerenderSpaPlugin({
